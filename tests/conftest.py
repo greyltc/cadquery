44230bd0c88fbaf12cf3ea22d936a8db06cb81b6
M. Greyson Christoforo <grey@christoforo.net>
2025-10-07 10:22:36 +0100

    rename gui test option to avoid conflict

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,21 +2,21 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--gui", action="store_true", default=False, help="run gui tests")
+    parser.addoption("--tgui", action="store_true", default=False, help="run gui tests")
 
 
 def pytest_configure(config):
-    config.addinivalue_line("markers", "gui: mark gui test")
+    config.addinivalue_line("markers", "tgui: mark gui test")
 
 
 def pytest_collection_modifyitems(config, items):
 
-    # run gui tests --gui option is proveded
-    if config.getoption("--gui"):
+    # run gui tests --tgui option is proveded
+    if config.getoption("--tgui"):
         return
 
     # skip gui tests otherwise
-    skip_gui = pytest.mark.skip(reason="need --gui option to run")
+    skip_gui = pytest.mark.skip(reason="need --tgui option to run")
     for item in items:
-        if "gui" in item.keywords:
+        if "tgui" in item.keywords:
             item.add_marker(skip_gui)
